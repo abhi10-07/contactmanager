a2e62d30829396e3767113b49977c9725bb710fd
abhi10-07 <abhi.figo.10@gmail.com>
2019-07-21 23:16:16 +0530

    commit 22-7v3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,25 +2,63 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false,
+    arrowIcon: "down"
+  };
+
+  //using arrow function, we do not need to bind the function
+  onShowClick = e => {
+    // console.log(e.target);
+    this.setState({
+      showContactInfo: !this.state.showContactInfo,
+      arrowIcon: this.state.arrowIcon !== "down" ? "down" : "up"
+    });
+  };
+
+  //this will help in changin state from this(another) component.
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  };
+
   render() {
     // destructuring
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.props.contact;
+    const { showContactInfo, arrowIcon } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name}
+          <i
+            onClick={this.onShowClick}
+            className={"fas fa-sort-" + arrowIcon}
+            style={{ cursor: "pointer" }}
+          />
+          <i
+            className="fas fa-times float-right text-danger"
+            style={{ cursor: "pointer" }}
+            onClick={this.onDeleteClick}
+          />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
 
 Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired
+  //if props are sent individually
+  // name: PropTypes.string.isRequired,
+  // email: PropTypes.string.isRequired,
+  // phone: PropTypes.string.isRequired
+  //if props are sent in object
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,39 +1,60 @@
 import React, { Component } from "react";
+import Contact from "./Contact";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "Luis Figo",
-          email: "figo@gmail.com",
-          phone: "10"
-        },
-        {
-          id: 2,
-          name: "Zinedine Zidane",
-          email: "Zidane@gmail.com",
-          phone: "05"
-        },
-        {
-          id: 3,
-          name: "Raul Gonzales",
-          email: "raul@gmail.com",
-          phone: "07"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Luis Figo",
+        email: "figo@gmail.com",
+        phone: "10"
+      },
+      {
+        id: 2,
+        name: "Zinedine Zidane",
+        email: "Zidane@gmail.com",
+        phone: "05"
+      },
+      {
+        id: 3,
+        name: "Raul Gonzales",
+        email: "raul@gmail.com",
+        phone: "07"
+      }
+    ]
+  };
+
+  //delete function to delete selected id and change the state
+  deleteContact = id => {
+    const { contacts } = this.state;
+
+    const newContact = contacts.filter((contact, index) => contact.id !== id);
+
+    this.setState({
+      contacts: newContact
+    });
+  };
+
   render() {
     const { contacts } = this.state;
     return (
-      <div>
+      <React.Fragment>
         {contacts.map(contact => (
-          <h1>{contact.name}</h1>
+          <Contact
+            key={contact.id}
+            //Sent individually
+            // name={contact.name}
+            // email={contact.email}
+            // phone={contact.phone}
+
+            //Sent in object
+            contact={contact}
+            //creating a handler which call defined delete function
+            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+          />
         ))}
-      </div>
+      </React.Fragment>
     );
   }
 }
